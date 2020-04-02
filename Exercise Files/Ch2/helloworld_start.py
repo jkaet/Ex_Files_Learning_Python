e7180969c6348159989440bfd887ead518392c87
JaQuan Taylor <jkaet@protonmail.com>
2020-04-02 08:10:37 -0400

    finsished c2 of refresher python course

--- Exercise Files/Ch2/helloworld_start.py
+++ Exercise Files/Ch2/helloworld_start.py
@@ -1,4 +1,7 @@
 #
 # Example file for HelloWorld
 #
-
+def main():
+    print("Hello World")
+if __name__ == "__main__":
+    main()
--- Exercise Files/Ch2/variables_start.py
+++ Exercise Files/Ch2/variables_start.py
@@ -1,15 +1,25 @@
-# 
+#
 # Example file for variables
 #
 
 # Declare a variable and initialize it
-
-
-# # re-declaring the variable works
-
-
-# # ERROR: variables of different types cannot be combined
-
+f=0
+# print(f)
+#
+# # # re-declaring the variable works
+# f = "abc"
+# print(f)
+#
+# # # ERROR: variables of different types cannot be combined
+# print("this is a string"+123)
 
 # Global vs. local variables in functions
+def someFunction():
+    global f
+    f="def"
+    print(f)
+someFunction()
+print(f)
 
+del(f)
+print(f)
